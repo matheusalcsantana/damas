8baf8ac5e350876bd3c4267a8d554189febd036a
zinhoinho <matheus-santana96@hotmail.com>
2017-12-14 09:20:57 -0300

    tabuleiro não finalizado

--- game.py
+++ game.py
@@ -3,18 +3,27 @@
 
 import pygame
 
-white = 255,255,255
 
-screen = pygame.display.set_mode((640,400))
+#cores
+white = 255,255,255
+tabuleiro1 = 51, 25, 0
+tabuleiro2 = 204, 153, 51
+#Tamanho da tela
+screen = pygame.display.set_mode((800,600))
 
 Exit = True
 
 while Exit:
+	
 	for event in pygame.event.get():
+		#Evento para quitar game
 		if event.type == pygame.QUIT:
 			Exit = False
-
+			
+	#Cor da tela (Branca)
 	screen.fill(white)
+	pygame.draw.rect(screen, tabuleiro1, [150,50,500,500])
+	#Atualizar a tela
 	pygame.display.flip()
 
 pygame.quit()

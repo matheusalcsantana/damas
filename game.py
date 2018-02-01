9f6a73b110a4f864f0b9fbfc51a9221b8540ce59
matheusalcsantana <matheus-santana96@hotmail.com>
2018-02-01 15:45:28 -0300

    matriz_tab and val_mouse

--- game.py
+++ game.py
@@ -10,8 +10,9 @@ white = 255,255,255
 black = 0,0,0
 tabuleiro1 = 183, 105, 56
 tabuleiro2 = 253, 239, 192
-pecas1 = 84, 40, 0
-pecas2 = 216, 210, 141
+cor_pretas = 84, 40, 0
+cor_brancas = 216, 210, 141
+verde = 0, 204, 0
 #Tamanho da tela
 screen = pygame.display.set_mode((800,600))
 #Tamanho tabuleiro e peças
@@ -22,61 +23,78 @@ r = 22
 yi = 100
 matriz_tab = []
 for i in range(8):
+	linha = []
 	if i % 2 == 0:
 		xi = 200 + l
-	else: 
+	else:
 		xi = 200
-		
+
+	if i >= 0 and i <= 2: aux = 'p'
+ 	elif i > 2 and i <= 4: aux = ' '
+	else: aux = 'b'
+
 	for j in range(4):
-			coord = (xi, yi)
-			matriz_tab.append(coord)
+			coord = [aux, xi, yi]
+			linha.append(coord)
 			xi += 2*l
+	matriz_tab.append(linha)
 	yi += l
-	
-print matriz_tab
+
 #Função desenhar peças
-def desenhar_pecas():
-	for i in range(12):
-		#Pecas1
-		pygame.draw.circle(screen, pecas1, [matriz_tab[i][0] + l/2, matriz_tab[i][1] + l/2], r)
-		pygame.draw.circle(screen, black, [matriz_tab[i][0] + l/2, matriz_tab[i][1] + l/2], r, 1)
-		pygame.draw.circle(screen, black, [matriz_tab[i][0] + l/2, matriz_tab[i][1] + l/2], 15, 1)
-		
-	for j in range(len(matriz_tab) - 1, len(matriz_tab) - 13, -1):
-		#Pecas2
-		pygame.draw.circle(screen, pecas2, [matriz_tab[j][0] + l/2, matriz_tab[j][1] + l/2], r)
-		pygame.draw.circle(screen, black, [matriz_tab[j][0] + l/2, matriz_tab[j][1] + l/2], r, 1)
-		pygame.draw.circle(screen, black, [matriz_tab[j][0] + l/2, matriz_tab[j][1] + l/2], 15, 1)
-			
-	
-		
+def desenhar_pecas(matriz):
+	for i in range(8):
+		for j in range(4):
+			#Peças brancas
+			if matriz_tab[i][j][0] == 'b':
+				pygame.draw.circle(screen, cor_brancas, [matriz[i][j][1] + l/2, matriz[i][j][2] + l/2], r)
+				pygame.draw.circle(screen, black, [matriz[i][j][1] + l/2, matriz[i][j][2] + l/2], r, 1)
+				pygame.draw.circle(screen, black, [matriz[i][j][1] + l/2, matriz[i][j][2] + l/2], 15, 1)
+
+			#Peças pretas
+			if matriz_tab[i][j][0] == 'p':
+				pygame.draw.circle(screen, cor_pretas, [matriz[i][j][1] + l/2, matriz[i][j][2] + l/2], r)
+				pygame.draw.circle(screen, black, [matriz[i][j][1] + l/2, matriz[i][j][2] + l/2], r, 1)
+				pygame.draw.circle(screen, black, [matriz[i][j][1] + l/2, matriz[i][j][2] + l/2], 15, 1)
+
+def val_mouse_click(c):
+	for i in range(8):
+		for j in range(4):
+			if (c[0] >= matriz_tab[i][j][1] and c[0] <= matriz_tab[i][j][1] + l) and (c[1] >= matriz_tab[i][j][2] and c[1] <= matriz_tab[i][j][2] + l):
+				if matriz_tab[i][j][0] == 'p' or matriz_tab[i][j][0] == 'b':
+					return True
+				else: return False
+	else: return False
+
 Exit = True
 
 while Exit:
-	
+
 	for event in pygame.event.get():
 		#Evento para quitar game
 		if event.type == pygame.QUIT:
 			Exit = False
 		if event.type == pygame.MOUSEBUTTONDOWN:
-			print MOUSEBUTTONDOWN	
+			mouse_pos = pygame.mouse.get_pos()
+			print mouse_pos
+			print val_mouse_click(mouse_pos)
+
 	#Cor da tela (Branca)
 	screen.fill(white)
 	#Retângulo grande (Tabuleiro 1)
 	pygame.draw.rect(screen, tabuleiro2, [200,100,400,400])
 	#Retangulos pequenos (Tabuleiro 2)
-	
-	for i in range(32):
-		xi = matriz_tab[i][0]
-		yi = matriz_tab[i][1]
-		pygame.draw.rect(screen, tabuleiro1, [xi, yi, l, l])
-	
-	desenhar_pecas()
+
+	for i in range(8):
+		for j in range(4):
+			xi = matriz_tab[i][j][1]
+			yi = matriz_tab[i][j][2]
+			pygame.draw.rect(screen, tabuleiro1, [xi, yi, l, l])
+
+	desenhar_pecas(matriz_tab)
 	#locais onde as peças podem ir
-	
+
 	#Atualizar a tela
 	pygame.display.update()
 
 pygame.quit()
 quit
-	
